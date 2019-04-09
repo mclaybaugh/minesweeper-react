0d5e49897add9866428cbb831cd44105eb53fd2d
Michael Claybaugh <mclaybaugh2@liberty.edu>
2019-04-09 14:20:57 -0400

    parcel with react js and css, babel, eslint

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import './index.css';
+
+class HelloMessage extends React.Component {
+  render() {
+    return (
+      <div>Hello {this.props.name}</div>
+    );
+  }
+}
+
+var mountNode = document.getElementById("app");
+ReactDOM.render(<HelloMessage name="Jane" />, mountNode);
